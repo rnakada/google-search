1b2a6d84477a47a0a5d9337637b974fc37d99e6b
unknown <rnakada90@gmail.com>
2019-04-21 21:03:17 -0400

    ohhhhhhman

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+// const routes = require("./routes/index");
 
-//Define middleware here 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
+    // Although this references the build folder... Use the public folder in client/public to publish images/css/any static file
     app.use(express.static("client/build"));
-};
+    // client/public is the actual folder to use for static files
+}
 
-//Add routes, both API and view
-app.use(routes)(app);
-// require("./routes")(app);
+// You really only need API routes and not any HTML routes if you are using REACTJS as the frontend
+// ******************************API ROUTES INCLUDED HERE***************************** //
+app.get("/_api/non-cached", (req, res) => {
+    res.json({ random: Math.random() });
+});
+app.get("/api/cached", (req, res) => {
+    res.json({ random: Math.random() });
+});
 
-//Request to React
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// Send every request to the React app
+// Define any API routes before this runs
+// will be broken in development...
+app.get("*", function (req, res) {
+    if (process.env.NODE_ENV === "production") {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    }
 });
 
 //Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:username90@ds145456.mlab.com:45456/heroku_s0gbdf0d", { useNewUrlParser: true });
 
-//Start the API server
-app.listen(PORT, () => {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
